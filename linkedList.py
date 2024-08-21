2cc3d49d68b4006584233afbfd203e93e08e7da7
nagasundharam <nagasundharam553@gmail.com>
2024-08-21 15:11:19 +0530

    linked list traversal and add_begin

---
+++ linkedList.py
@@ -0,0 +1,91 @@
+# LinkedList :
+# linked list contains node
+
+#  Each  node contains  data and next  or reference to the next node
+  
+
+# we dont need to store a specific part of the memory or contagious memory allocation
+
+# linked list it is dynamic memory allocation 
+
+
+# advantages:
+#     - dynamic
+#     - easily insertable or deletable
+#     - you can implement stack , queue and graph
+#     it is used in web browser  and as well as  memory
+# real time usage  -- we browser 
+                    # -- music Player
+                    # -- Image viewer
+
+
+# disadvantages
+       
+    #    -- Needs extra memory
+    #   --  Random access not possible
+    
+
+# LINKEDLIST TYPES:
+    #  --- Singly LinkedList
+    #  --- doubly LinkedList
+    #  --- circular LinkedList
+
+# Singly Linked List
+
+# Add/Insertion
+#   --> Begin
+#   --> end
+#   --> inbetween   
+
+
+# Linked list creating
+
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.ref = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    # traversal operation
+    def print_LL(self):
+        if self.head is None:
+            print("list is empty")
+        else:
+            n = self.head
+            while n is not None:
+                print(n.data)
+                n = n.ref
+        
+    def add_begin(self,data):
+        new_node = Node(data)
+        new_node.ref = self.head
+        self.head = new_node
+    
+
+   
+       
+
+ll1 = LinkedList()
+ll1.add_begin(10)
+ll1.add_begin(20)
+ll1.add_begin(30)
+ll1.add_begin(40)
+ll1.print_LL()
+    
+
+   
+
+# node1 = Node(10)
+# print(node1)  #<__main__.Node object at 0x00000145081561B0>
+
+
+
+  
+
+
+
+
