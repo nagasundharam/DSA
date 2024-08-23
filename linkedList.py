2cc97b5fd9f2007fad91ba2185c011ce20b76767
nagasundharam <nagasundharam553@gmail.com>
2024-08-23 18:32:02 +0530

    linkedlist deleting

--- linkedList.py
+++ linkedList.py
@@ -122,6 +122,54 @@ class LinkedList:
         else:
             print("Linked list is not empty")
 
+
+    # delete elements
+
+
+    def delete_begin(self):
+        if self.head is None:
+            print("LL is empty we cant delete any further ")
+            
+        else:
+            self.head = self.head.ref 
+    
+    def delete_end(self):
+        if self.head is None :
+            print("LL is empty")
+        else :
+            n = self.head
+            while n is not None:
+
+                if n.ref.ref  == None :
+                    break
+                n = n.ref
+            n.ref = None
+    
+    def delete_between(self,x):
+        if self.head is None:
+            print("LL is empty")
+        elif (self.head.data  == x):
+            self.head = self.head.ref
+
+
+        else:
+            n = self.head
+            while n.ref is not None :
+                if (n.ref.data ==  x):
+                    break
+                n = n.ref
+            if n.ref is None:
+                print("there is no element in LL")
+            else:
+                n.ref = n.ref.ref
+            
+            
+        
+
+    
+    
+
+
         
 
         
@@ -141,8 +189,14 @@ class LinkedList:
        
 
 ll1 = LinkedList()
-ll1.insert_empty(10)
-ll1.insert_empty(20)
+ll1.insert_empty(700)
+ll1.add_begin(20)
+ll1.add_begin(30)
+ll1.add_begin(50)
+
+
+
+ll1.delete_between(100)
 # ll1.after_node(80,100)
 
 ll1.print_LL()
